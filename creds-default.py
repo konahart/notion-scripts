d917062bdf29ee65a3a8800a535b5b40429159f6
Kona Goodhart <code@konahart.com>
2021-02-02 00:14:17 -0800

    Add note about how to get notion token

--- creds-default.py
+++ creds-default.py
@@ -8,6 +8,9 @@ PINTEREST = {
     'username': 'YOUR USERNAME HERE'
 }
 
+
+# Obtain your Notion token by inspecting your browser cookies on a logged-in
+# (non-guest) session on Notion.so, copy token_v2 below
 NOTION = {
     'token': 'YOUR NOTION TOKEN HERE'
 }
