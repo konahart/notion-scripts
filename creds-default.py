063af4c6a6cf6fca51d85729c60e4a37864445cc
Kona Goodhart <code@konahart.com>
2021-02-02 00:30:31 -0800

    Move Notion credentials to top of creds file

--- creds-default.py
+++ creds-default.py
@@ -2,14 +2,6 @@
 Put your creds, and rename the file to creds.py
 DO NOT commit creds.py to the repo!
 """
-PINTEREST = {
-    'email': 'YOUR EMAIL HERE',
-    'password': 'YOUR PASSWORD HERE',
-    'username': 'YOUR USERNAME HERE',
-    'default_board_id': "OPTIONAL: DEFAULT BOARD ID HERE",
-    'default_board_name': "OPTIONAL: DEFAULT BOARD NAME HERE"
-}
-
 
 # Obtain your Notion token by inspecting your browser cookies on a logged-in
 # (non-guest) session on Notion.so, copy token_v2 below
@@ -17,6 +9,14 @@ NOTION = {
     'token': 'YOUR NOTION TOKEN HERE',
     'default_page_url': 'OPTIONAL: DEFAULT PAGE URL HERE',
     'default_page_title': 'OPTIONAL: DEFAULT PAGE TITLE HERE'
+}
+
+PINTEREST = {
+    'email': 'YOUR EMAIL HERE',
+    'password': 'YOUR PASSWORD HERE',
+    'username': 'YOUR USERNAME HERE',
+    'default_board_id': "OPTIONAL: DEFAULT BOARD ID HERE",
+    'default_board_name': "OPTIONAL: DEFAULT BOARD NAME HERE"
 }
 
 TWITTER = {
