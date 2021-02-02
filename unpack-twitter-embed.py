d681695d80d8089b516ce740ce05916f811f726f
Kona Goodhart <code@konahart.com>
2021-02-02 00:42:45 -0800

    Set up tweepy auth

--- unpack-twitter-embed.py
+++ unpack-twitter-embed.py
@@ -1,14 +1,22 @@
+import tweepy
 from notion.client import NotionClient
 
-from creds import NOTION
+from creds import NOTION, TWITTER
 
 
 def main(page_url, page_title):
-    client = NotionClient(token_v2=NOTION['token'])
-    page = client.get_block(page_url)
+    # set up clients for notion and twitter
+    notion = NotionClient(token_v2=NOTION['token'])
+    twitter_auth = tweepy.OAuthHandler(
+        TWITTER['consumer_key'], TWITTER['consumer_secret'])
+    twitter_auth.set_access_token(
+        TWITTER['access_key'], TWITTER['access_secret'])
+    twitter = tweepy.API(twitter_auth)
+
+    page = notion.get_block(page_url)
     if page.title != page_title:
-        print(
-            f"sanity check: {page.title} does not expected title: {page_title}")
+        print(f"sanity check: {page.title} does not expected title: "
+              "{page_title}")
         return
     rows = page.collection.get_rows()
     print(f"found {len(rows)} records")
