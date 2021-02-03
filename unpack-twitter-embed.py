152dd6e4da01d031eea0caa623bd784f9999fbb8
Kona Goodhart <code@konahart.com>
2021-02-02 23:34:48 -0800

    Separate out tweet logic

--- unpack-twitter-embed.py
+++ unpack-twitter-embed.py
@@ -9,6 +9,20 @@ def get_tweet_id_from_url(tweet_url):
     return tweet_url.split('/photo')[0].split('/')[-1].split('?')[0]
 
 
+def get_tweet_image_urls(twitter_client, tweet_url):
+    tweet_id = get_tweet_id_from_url(tweet_url)
+    print(f"{tweet_url} - {tweet_id}")
+    tweet = twitter_client.get_status(tweet_id)
+    entities = getattr(tweet, "extended_entities", None) or getattr(
+        tweet, "entities", None)
+    media = entities.get('media')
+    urls = []
+    if media:
+        for image in media:
+            urls.append(image['media_url_https'])
+    return urls
+
+
 def main(page_url, page_title):
     # set up clients for notion and twitter
     notion = NotionClient(token_v2=NOTION['token'])
@@ -28,16 +42,10 @@ def main(page_url, page_title):
     for row in rows:
         types = {child.type: child for child in row.children}
         if "tweet" in types and "image" not in types:
-            id = get_tweet_id_from_url(types['tweet'].source)
-            tweet = twitter.get_status(id)
-            entities = getattr(tweet, "extended_entities", None) or getattr(
-                tweet, "entities", None)
-            media = entities.get('media')
-            if media:
-                for image in media:
-                    url = image['media_url_https']
-                    newchild = row.children.add_new(ImageBlock)
-                    newchild.set_source_url(url)
+            image_urls = get_tweet_image_urls(twitter, types['tweet'].source)
+            for url in image_urls:
+                newchild = row.children.add_new(ImageBlock)
+                newchild.set_source_url(url)
 
 
 if __name__ == "__main__":
