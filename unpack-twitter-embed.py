1bf833a1dde3536a6e86444743818739c7337a5d
Kona Goodhart <code@konahart.com>
2021-03-13 21:34:39 -0800

    Create multiple rows if multiple images

--- unpack-twitter-embed.py
+++ unpack-twitter-embed.py
@@ -1,6 +1,6 @@
 import tweepy
 from notion.client import NotionClient
-from notion.block import ImageBlock
+from notion.block import EmbedBlock, ImageBlock
 
 from creds import NOTION, TWITTER
 
@@ -47,13 +47,31 @@ def main(page_url, page_title):
         return
     rows = page.collection.get_rows()
     print(f"found {len(rows)} records")
+    new_rows = []
     for row in rows:
         types = {child.type: child for child in row.children}
         if "tweet" in types and "image" not in types:
+            print(get_notion_url_from_id(row.id))
             image_urls = get_tweet_image_urls(twitter, types['tweet'].source)
-            for url in image_urls:
-                newchild = row.children.add_new(ImageBlock)
-                newchild.set_source_url(url)
+            for i, url in enumerate(image_urls):
+                if i == 0:
+                    newchild = row.children.add_new(ImageBlock)
+                    newchild.set_source_url(url)
+                else:
+                    new_rows.append((row, url))
+    print(f"{len(new_rows)} new rows to create")
+    for (row, imgurl) in new_rows:
+        new_row = page.collection.add_row()
+        new_row.url = row.url
+        new_row.created = row.created
+        new_row.tags = row.tags
+        newchild = new_row.children.add_new(EmbedBlock)
+        try:
+            newchild.set_source_url(new_row.url)
+        except KeyError:
+            pass
+        newchild = new_row.children.add_new(ImageBlock)
+        newchild.set_source_url(imgurl)
 
 
 if __name__ == "__main__":
